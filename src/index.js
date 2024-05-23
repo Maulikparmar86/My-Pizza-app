c64a36a2dee89bc809f7bd932d2c7b4299935551
Maulikparmar86 <145836101+Maulikparmar86@users.noreply.github.com>
2024-05-23 15:43:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '@aws-amplify/ui-react/styles.css';
 
 import Amplify from 'aws-amplify';
 import awsconfig from './aws-exports';
